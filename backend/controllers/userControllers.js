9756d99d6e56187f39424ae24e490dd7f44e0cad
Harshit <harshpandeyalfa2@gmail.com>
2023-08-29 11:05:42 +0530

    no changes

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -17,6 +17,7 @@ const allUsers = asyncHandler(async (req, res) => {
 
   const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
   res.send(users);
+  // return next();
 });
 
 //@description     Register new user
